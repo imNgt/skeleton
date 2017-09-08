1ba425f70c95bade82b2f641df5fb9151bc8ff95
nongguitao <nongguitao@qgutech.com>
2017-09-08 16:53:57 +0800

    page

--- dist/js/skeleton.js
+++ dist/js/skeleton.js
@@ -1,6 +1,6 @@
 /**
  * skeleton.js
- * build at: Thu Sep 07 2017 18:18:49 GMT+0800 (中国标准时间)
+ * build at: Fri Sep 08 2017 16:37:26 GMT+0800 (中国标准时间)
  */
 if (typeof jQuery === 'undefined') {throw new Error('jQuery is required')};
 ;(function($,window){
@@ -34,90 +34,137 @@ if (typeof jQuery === 'undefined') {throw new Error('jQuery is required')};
 *current 当前页数
 *middlePage 渲染的中间页数
 *onChange 页码改变的回调
-	*/
+*/
 ; (function (Skeleton, $) {
-
-	var index = 0;//实例化组件数	
+	//实例化组件数	
+	var index = 0;
 
 	var Page = function (options) {
+
 		var config = {
-			elem: '', 
-			total: 999,
-			current: 10,
-			middlePage: 5, 
-			pageSize:10   
+			elem: '',
+			total: 360,
+			current: 5,
+			middlePage: 5,
+			pageSize: 10,
+			next: "下一页",
+			prev: "上一页"
 		};
+
 
 		this.config = $.extend(config, options || {});
 
+		this.elem = $(this.config.elem);
 
-		$(this.config.elem).html(this.calculatePages().join(""));
+		this.renderPage();
+
+		this.bindEvent();
 	}
+
+	var CLASS_ITEM = "page-item", CLASS_DOT = "page-dot", CLASS_CURR = "page-item page-curr", CLASS_PREV = "page-prev", CLASS_NEXT = "page-next", CLASS_DISABLE = "page-disabled";
 
 	Page.inherits(Skeleton);
 
-	// 计算页码，返回页码元素    
 	Page.prototype = {
+		/* 计算页码，返回页码元素     */
 		calculatePages: function () {
-			var that=this;
+			var that = this;
 			var config = that.config,
 				current = config.current,
 				middlePage = config.middlePage,
 				pageSize = config.pageSize,
-				total = config.total;    
-			var paginalNumber = Math.ceil(total / pageSize);
-			var fixPage = 0,									// 根据middlePage修正页码
-				viewPageStart = 0,									// 中间页码开始
-				viewPageEnd = 0,									// 中间页码结束
-				ret = [];
+				total = config.total,
+				pages = Math.ceil(total / pageSize),
+				viewPageStart = 0,
+				viewPageEnd = 0,
+				resutl = [];
 
-			// 中间页码修正，middlePage为偶数，viewPageEnd需要减1
-			fixPage = middlePage & 1 == 0 ? 1 : 0;
-
-			// 检测前边界值 
-			if (current <= middlePage) {
+			/*检测边界值  */
+			if (current < middlePage) {
 				viewPageStart = 1;
-				viewPageEnd = middlePage + Math.floor(middlePage / 2);
-			} else if (current >= paginalNumber - Math.floor(middlePage / 2) - 1) {	// -1 是为了最后一页显示6条分页数据 32..33 是不合理的 应该是 32 33
-				// 检测后边界值
-				viewPageStart = paginalNumber - middlePage;
-				viewPageEnd = paginalNumber; 
+				viewPageEnd = middlePage;
+			} else if (current >= pages - Math.ceil(middlePage / 2)) {
+				viewPageStart = pages - middlePage;
+				viewPageEnd = pages;
 			} else {
-				// middlePage在中间
+				/* middlePage在中间 */
 				viewPageStart = current - Math.floor(middlePage / 2);
-				viewPageEnd = current + Math.floor(middlePage / 2) - fixPage;
+				viewPageEnd = current + Math.floor(middlePage / 2);
+				!(middlePage & 1) && viewPageEnd--;
 			}
 			/*limit viewPageEnd*/
-			if (viewPageEnd > paginalNumber) {
-				viewPageEnd = paginalNumber;
+			if (viewPageEnd > pages) {
+				viewPageEnd = pages;
 			}
 			/*show dot near start*/
 			if (viewPageStart > 2) {
-				ret.push('<span key="page-first">1</span>');
-				ret.push('<span key="page-first-dot" class="page-dot">...</span>');
+				resutl.push('<span class="' + CLASS_ITEM + '">1</span>');
+				resutl.push('<span class="' + CLASS_DOT + '">...</span>');
 			}
 			/*view of middle*/
-			that.range(viewPageStart, viewPageEnd + 1).map(function(index, key){
-				var currClass=index == current ? "page-current" :"";   
-				ret.push('<span key="page"  class="'+currClass +'">'+index+'</span>');
+			that.range(viewPageStart, viewPageEnd + 1).map(function (index, key) {
+				resutl.push('<span   class="' + (index == current ? CLASS_CURR : CLASS_ITEM) + '">' + index + '</span>');
 			});
-                                      
- 
-			/*show dot near end*/   
-			if (viewPageEnd != paginalNumber) {
-				ret.push('<span key="page-last" class="page-dot">...</span>');
-				ret.push('<span key="page-last-dot" >'+paginalNumber+'</span>');
+
+			/*show dot near end*/
+			if (viewPageEnd != pages) {
+				resutl.push('<span  class="' + CLASS_DOT + '">...</span>');
+				resutl.push('<span  class="' + CLASS_ITEM + '">' + pages + '</span>');
 			}
-			return ret;     
+
+			return resutl;
 		},
-		range:function(start , stop , step) {
-			var start=start||0,step=step||1;
-			var length = Math.max(Math.ceil((stop - start) / step), 0); 
-			var range = Array(length);     
-			for(var idx = 0; idx < length; idx++, start += step) {
-				range[idx] = start; 
-			}       
+		range: function (start, stop) {
+			var start = start || 0, i, length = stop - start, range = Array(length);
+			for (i = 0; i < length; i++ , start++) {
+				range[i] = start;
+			}
 			return range;
+		},
+		bindEvent: function () {
+			var that = this;
+			that.elem.on("click", "." + CLASS_ITEM, function () {
+				var target = this.innerHTML * 1;
+				that.jumpPage(target);
+			})
+		},
+		jumpPage: function (target) {
+			if (target == this.config.current) {
+				return;
+			}
+			this.config.current = target;
+			this.renderPage();
+			this.handleChange();
+		},
+		renderPage: function () {
+			var template = [],
+			prev = this.config.prev,
+			next = this.config.next,
+			current = this.config.current,
+			pages = this.config.pages;
+   
+			if (prev) {
+				var prevClass = current == 1 ? CLASS_DISABLE : CLASS_PREV;
+				template.push('<span  class="' + prevClass + '">' + prev + '</span>');
+			}
+
+			template = template.concat(this.calculatePages());
+
+			if (next) {
+				var nextClass = current == pages ? CLASS_DISABLE : CLASS_NEXT;
+				template.push('<span  class="' + nextClass + '">' + next + '</span>');
+			}
+			
+			this.elem.html(template.join(""));   
+		},
+		handleChange: function () {
+			if (typeof this.config.onChange === "function") {
+				this.config.onChange.call(this, {
+					current: this.config.current,
+					total: this.config.total,
+					size: this.config.pageSize
+				});
+			}
 		}
 	}
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 const path = require('path'),
-gulp = require('gulp'),
-sass = require('gulp-sass'),
-autoPreFixer = require('gulp-autoprefixer'),
-minifyCss = require('gulp-minify-css'),
-jshint = require('gulp-jshint'),
-uglify = require('gulp-uglify'),
-rename = require('gulp-rename'),
-concat = require('gulp-concat'),
-notify = require('gulp-notify'),
-header = require('gulp-header'),
-liveReload = require('gulp-livereload'),
-cleanCSS = require('gulp-clean-css');
+	gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	autoPreFixer = require('gulp-autoprefixer'),
+	minifyCss = require('gulp-minify-css'),
+	jshint = require('gulp-jshint'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	notify = require('gulp-notify'),
+	header = require('gulp-header'),
+	liveReload = require('gulp-livereload'),
+	cleanCSS = require('gulp-clean-css');
 
 //路径配置
 let SRCPATH = path.resolve(__dirname, 'source');
@@ -18,56 +18,55 @@ let DISTPATH = path.resolve(__dirname, 'dist');
 
 //解析sass
 gulp.task('sass', function () {
-return gulp.src([
-	SRCPATH + '/scss/main.scss'
-])
-	.pipe(sass())
-	.on("error", notify.onError({
-		message: "Error: <%= error.message %>"
-	}))
-	.pipe(autoPreFixer('last 101 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-	.pipe(gulp.dest(SRCPATH + "/css"))
-	.pipe(notify({message: 'sass parse task complete'}));
+	return gulp.src([
+		SRCPATH + '/scss/main.scss'
+	])
+		.pipe(sass())
+		.on("error", notify.onError({
+			message: "Error: <%= error.message %>"
+		}))
+		.pipe(autoPreFixer('last 101 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+		.pipe(gulp.dest(SRCPATH + "/css"))
+		.pipe(notify({ message: 'sass parse task complete' }));
 });
 
 //压缩css
 gulp.task('mincss', ['sass'], function () {
-return gulp.src([
-	SRCPATH + '/css/main.css',
-	SRCPATH + '/css/compatibility.css'
-])
-	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(header('/**\n * <%= file.relative %>\n * build at: <%= new Date() %>\n */\n'))
-	.pipe(gulp.dest(DISTPATH + '/css/'))
-	.pipe(notify({message: 'min css task complete'}))
+	return gulp.src([
+		SRCPATH + '/css/main.css',
+		SRCPATH + '/css/compatibility.css'
+	])
+		.pipe(cleanCSS({ compatibility: 'ie8' }))
+		.pipe(header('/**\n * <%= file.relative %>\n * build at: <%= new Date() %>\n */\n'))
+		.pipe(gulp.dest(DISTPATH + '/css/'))
+		.pipe(notify({ message: 'min css task complete' }))
 });
 
 gulp.task('scripts', function () {
-return gulp.src([
-	SRCPATH + "/js/components/skeleton.js",
-	SRCPATH + "/js/components/page.js",
-	SRCPATH + "/js/components/select.js"      
-]).pipe(concat('skeleton.js'))
-	.pipe(header('/**\n * <%= file.relative %>\n * build at: <%= new Date() %>\n */\n'))
-	.pipe(gulp.dest(DISTPATH + "/js/"))
-	.pipe(rename("skeleton.min.js"))
-	.pipe(uglify({
-		mangle: true,
-		toplevel :true   
-	}))
-	.on("error", notify.onError({
-		message: "Error: <%= error.message %>"
-	}))
-	.pipe(gulp.dest(DISTPATH + '/js/'))
-	.pipe(notify({message: 'Scripts task complete'}));
+	return gulp.src([
+		SRCPATH + "/js/components/skeleton.js",
+		SRCPATH + "/js/components/page.js",
+		SRCPATH + "/js/components/select.js"
+	]).pipe(concat('skeleton.js'))
+		.pipe(header('/**\n * <%= file.relative %>\n * build at: <%= new Date() %>\n */\n'))
+		.pipe(gulp.dest(DISTPATH + "/js/"))
+		.pipe(rename("skeleton.min.js"))
+		.pipe(uglify({
+			mangle: true,
+			toplevel: true
+		}))
+		.on("error", notify.onError({
+			message: "Error: <%= error.message %>"
+		}))
+		.pipe(gulp.dest(DISTPATH + '/js/'))
+		.pipe(notify({ message: 'Scripts task complete' }));
 });
 
 //文件修改监听
 gulp.task('watch', function () {
-gulp.watch(SRCPATH + '/scss/**/*.scss', ['sass']);
-gulp.watch(SRCPATH + '/css/*.css', ['mincss']);
-gulp.watch(SRCPATH + '/js/**/*.js', ['scripts']);
-liveReload.listen();
-gulp.watch(SRCPATH + '**').on('change', liveReload.changed);
+	gulp.watch(SRCPATH + '/scss/**/*.scss', ['sass']);
+	gulp.watch(SRCPATH + '/css/*.css', ['mincss']);
+	gulp.watch(SRCPATH + '/js/**/*.js', ['scripts']);
+	liveReload.listen();
+	gulp.watch(SRCPATH + '**').on('change', liveReload.changed);
 });
- 
