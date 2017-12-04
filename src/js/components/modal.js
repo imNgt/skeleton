7b01c0a1a16c2c7a4e41d2dd29fb687ba5aee9ab
nongguitao <nongguitao@qgutech.com>
2017-12-04 17:54:31 +0800

    select

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,189 +1,192 @@
-; (function (Skeleton, $) {
+;
+(function (Skeleton, $) {
 
-	var index = 0;//实例化组件数	
-	var ZIndex = 9999;
+    var index = 0; //实例化组件数	
+    var ZIndex = 9999;
 
-	var Modal = function (options) {
-		var config = {
-			content: '',
-			shade: .1,
-			skin: 'modal',
-			width: "400px",
-			height: "",
-			title: "提示",
-			coexist: false,
-			ZIndex: false,
-			fixed: true,
-			shadowClose: true,
-			buttonClose: true,
-			button: ["取消", "确定"],
-			onButton1: function (modal) {
-				this.close();
-			},
-			onButton2: function (modal) {
-				this.close();
-			},
-			onLoad: function (modal) {
-				console.log(this, modal)
-			}
+    var Modal = function (options) {
+        var config = {
+            content: '',
+            shade: .1,
+            skin: 'modal',
+            width: "400px",
+            height: "",
+            title: "提示",
+            coexist: false,
+            ZIndex: false,
+            fixed: true,
+            shadowClose: true,
+            buttonClose: true,
+            button: ["取消", "确定"],
+            onButton1: function (modal) {
+                this.close();
+            },
+            onButton2: function (modal) {
+                this.close();
+            },
+            onLoad: function (modal) {
+                console.log(this, modal)
+            }
 
-		};
-		this.config = $.extend(config, options || {});
-		this.index = ++index;
+        };
+        this.config = $.extend(config, options || {});
+        this.index = ++index;
 
 
-		if (!this.config.coexist) {
-			this.closeAll();
-		}
+        if (!this.config.coexist) {
+            this.closeAll();
+        }
 
-		this.init();
-	}
+        this.init();
+    }
 
-	Modal.inherits(Skeleton);
+    Modal.inherits(Skeleton);
 
-	Modal.prototype = {
-		init: function () {
-			var config = this.config;
+    Modal.prototype = {
+        init: function () {
+            var config = this.config;
 
-			this.create();
-			this.setZIndex();
-			this.position();
-			this.bindEvent();
+            this.create();
+            this.setZIndex();
+            this.position();
+            this.bindEvent();
 
-			if (typeof config.onLoad === "function") {
-				config.onLoad.call(this, this.modal)
-			}
+            if (typeof config.onLoad === "function") {
+                config.onLoad.call(this, this.modal)
+            }
 
-			if (config.shade) {
-				this.shade.show();
-			}
-			this.modal.show();
+            if (config.shade) {
+                this.shade.show();
+            }
+            this.modal.show();
 
-		},
-		create: function () {
-			var config = this.config, i, button = config.button, temp = "";
+        },
+        create: function () {
+            var config = this.config,
+                i, button = config.button,
+                temp = "";
 
-			if (config.shade) {
-				temp += '<div class="sk-modal-shade modal-shade-' + this.index + '" key="' + this.index + '"></div>';
-			}
+            if (config.shade) {
+                temp += '<div class="sk-modal-shade modal-shade-' + this.index + '" key="' + this.index + '"></div>';
+            }
 
-			temp += '<div class="sk-modal sk-modal-' + this.index + ' ' + config.skin;
-			if (config.width) {
-				temp += '" style="width:' + config.width;
-			}
-			if (config.height) {
-				temp += ';height:' + config.height;
-			}
-			temp += '" key="' + this.index + '">';
+            temp += '<div class="sk-modal sk-modal-' + this.index + ' ' + config.skin;
+            if (config.width) {
+                temp += '" style="width:' + config.width;
+            }
+            if (config.height) {
+                temp += ';height:' + config.height;
+            }
+            temp += '" key="' + this.index + '">';
 
-			temp += '<div class="modal-head">' + config.title ;
-			if (config.buttonClose) {
-				temp += '<a href="javascript:;" class="modal-close"></a>';
-			}
-			temp += '</div>';
+            temp += '<div class="modal-head">' + config.title;
+            if (config.buttonClose) {
+                temp += '<a href="javascript:;" class="modal-close"></a>';
+            }
+            temp += '</div>';
 
-			temp += '<div class="modal-body">' + config.content + '</div>';
+            temp += '<div class="modal-body">' + config.content + '</div>';
 
-			if (button && button.length > 0) {
-				temp += '<div class="modal-footer">';
-				for (i = 0; i < button.length; i++) {
-					var key = i + 1;
-					temp += '<button type="button" class="btn modal-btn modal-btn-"' + key + '" key="' + key + '">' + button[i] + '</button>';
-				}
-				temp += '</div>';
-			}
-			temp += '</div>';
+            if (button && button.length > 0) {
+                temp += '<div class="modal-footer">';
+                for (i = 0; i < button.length; i++) {
+                    var key = i + 1;
+                    temp += '<button type="button" class="btn modal-btn modal-btn-"' + key + '" key="' + key + '">' + button[i] + '</button>';
+                }
+                temp += '</div>';
+            }
+            temp += '</div>';
 
-			$('body').append($(temp));
+            $('body').append($(temp));
 
-			this.modal = $("body").find(".sk-modal-" + this.index);
-			if (config.shade) {
-				this.shade = $("body").find(".modal-shade-" + this.index);
-			}
+            this.modal = $("body").find(".sk-modal-" + this.index);
+            if (config.shade) {
+                this.shade = $("body").find(".modal-shade-" + this.index);
+            }
 
-		},
-		position: function () {
-			var winWdith = $(window).width(),
-				winHeight = $(window).height(),
-				width = this.modal.outerWidth(),
-				height = this.modal.outerHeight();
+        },
+        position: function () {
+            var winWdith = $(window).width(),
+                winHeight = $(window).height(),
+                width = this.modal.outerWidth(),
+                height = this.modal.outerHeight();
 
-			var left = (winWdith - width) / 2, top = (winHeight - height) / 2;
+            var left = (winWdith - width) / 2,
+                top = (winHeight - height) / 2;
 
-			//绝对定位，加上滚动条卷去的高度	
-			if (!this.config.fixed) {
-				var scrollTop = document.documentElement.scrollTop + document.body.scrollTop;
-				var scrollLeft = document.documentElement.scrollLeft + document.body.scrollLeft;
-				left += scrollLeft;
-				top += scrollTop;
-				this.modal.css({ position: "absolute" });
-			}
+            //绝对定位，加上滚动条卷去的高度	
+            if (!this.config.fixed) {
+                var scrollTop = document.documentElement.scrollTop + document.body.scrollTop;
+                var scrollLeft = document.documentElement.scrollLeft + document.body.scrollLeft;
+                left += scrollLeft;
+                top += scrollTop;
+                this.modal.css({ position: "absolute" });
+            }
 
-			this.modal.css({ "left": left, "top": top });
-		},
-		setZIndex: function () {
-			var config = this.config;
-			if (config.shade) {
-				this.shade.css({ "z-index": ZIndex + index })
-			}
-			this.modal.css({ "z-index": ++ZIndex + index })
-		},
-		close: function () {
-			var that = this, config = this.config;
-			that.modal.addClass("fadeOutZoom_SK");
-			if (config.shade) {
-				that.shade.addClass("transparent");
-			}
-			setTimeout(function () {
-				that.modal.remove()
-				if (config.shade) {
-					that.shade.remove();
-				}
-			}, 300);
-			index--;
-			ZIndex--;
-		},
-		closeAll: function () {
-			$(".sk-modal-shade,.sk-modal").remove();
-			index = 0;
-			ZIndex = 9999;
-		},
-		bindEvent: function () {
-			var that = this,
-				config = this.config,
-				button = this.config.button;
-			that.modal.on("click", ".modal-head .modal-close", function () {
-				that.close();
-			})
+            this.modal.css({ "left": left, "top": top });
+        },
+        setZIndex: function () {
+            var config = this.config;
+            if (config.shade) {
+                this.shade.css({ "z-index": ZIndex + index })
+            }
+            this.modal.css({ "z-index": ++ZIndex + index })
+        },
+        close: function () {
+            var that = this,
+                config = this.config;
+            that.modal.addClass("fadeOutZoom_SK");
+            if (config.shade) {
+                that.shade.addClass("transparent");
+            }
+            setTimeout(function () {
+                that.modal.remove()
+                if (config.shade) {
+                    that.shade.remove();
+                }
+            }, 300);
+            index--;
+            ZIndex--;
+        },
+        closeAll: function () {
+            $(".sk-modal-shade,.sk-modal").remove();
+            index = 0;
+            ZIndex = 9999;
+        },
+        bindEvent: function () {
+            var that = this,
+                config = this.config,
+                button = this.config.button;
+            that.modal.on("click", ".modal-head .modal-close", function () {
+                that.close();
+            })
 
-			if (config.shadowClose) {
-				$(".sk-modal-shade.modal-shade-" + that.index).on("click", function () {
-					that.close();
-				})
-			}
+            if (config.shadowClose) {
+                $(".sk-modal-shade.modal-shade-" + that.index).on("click", function () {
+                    that.close();
+                })
+            }
 
-			if (button && button.length > 0) {
-				that.modal.find(".modal-btn").click(function () {
-					var key = this.getAttribute("key");
-					for (var i = 1; i <= button.length; i++) {
-						if (key == i && typeof config["onButton" + i] === "function") {
-							config["onButton" + i].call(that, that.modal)
-						}
-					}
-				})
-			}
+            if (button && button.length > 0) {
+                that.modal.find(".modal-btn").click(function () {
+                    var key = this.getAttribute("key");
+                    for (var i = 1; i <= button.length; i++) {
+                        if (key == i && typeof config["onButton" + i] === "function") {
+                            config["onButton" + i].call(that, that.modal)
+                        }
+                    }
+                })
+            }
 
-			$(window).resize(function () {
-				that.position();
-			})
+            $(window).resize(function () {
+                that.position();
+            })
 
-		}
-	}
+        }
+    }
 
-	Skeleton.modal = function (options) {
-		return new Modal(options)
-	}
+    Skeleton.modal = function (options) {
+        return new Modal(options)
+    }
 
 })(Skeleton, jQuery);
-
-
--- src/js/components/select.js
+++ src/js/components/select.js
@@ -1,190 +1,198 @@
-;
 (function (Skeleton, $) {
 
-	var index = 0; //实例化组件数	
+    var index = 0 //实例化组件数	
 
-	var Select = function (options) {
-		var config = {
-			elem: '', //select 元素
-			index: '', //实例化组件序号
-			onChange: function () { //回调函数
+    var Select = function (options) {
+        /* 默认配置
+         *elem:select 元素
+         *index:实例化组件序号
+         *onChange:选择后的回调函数
+         */
+        var config = {
+            elem: '',
+            index: '',
+            onChange: function () {}
+        }
+        this.config = $.extend(config, options || {})
+        this.elem = $(this.config.elem).eq(0)
+        this.index = index++
+		this.init()
+    }
+    /*私有变量 */
+    var CLASS_LIST = "sk-select-list",
+        CLASS_STATUS = "select-status",
+        CLASS_ACTIVE = "opened",
+        CLASS_TEXT = "selected-text",
+        CLASS_FADEINUP = "fadeInUp_SK"
 
-			}
-		};
-		this.config = $.extend(config, options || {});
-		this.elem = $(this.config.elem).eq(0);
-		this.index = index++;
-		this.init();
-	}
+    Select.prototype = {
+        constructor: Select,
+        init: function () {
+            this.buildTemplate()
+            this.bindEvent()
+            this.setDefaultOpt()
+        },
 
-	var CLASS_LIST = "sk-select-list",
-		CLASS_STATUS = "select-status",
-		CLASS_ACTIVE = "opened",
-		CLASS_TEXT = "selected-text",
-		CLASS_FADEINUP = "fadeInUp_SK";
+        getDefaultOpt: function () {
+            var that = this,
+                defaultOpt = {}
+            this.elem.find("option").each(function (k, opt) {
+                if ($(opt).attr("selected") !== undefined) {
+                    defaultOpt.value = opt.innerHTML
+                    defaultOpt.index = that.selectedIndex = k
+                    return
+                }
+            })
 
-	Select.prototype = {
-		constructor: Select,
-		init: function () {
-			this.buildTemplate();
-			this.bindEvent();
-			this.setDefaultOpt();
-		},
+            return defaultOpt
+        },
 
-		getDefaultOpt: function () {
-			var that = this,
-				defaultOpt = {};
-			this.elem.find("option").each(function (k, opt) {
-				if ($(opt).attr("selected") !== undefined) {
-					defaultOpt.value = opt.innerHTML;
-					defaultOpt.index = that.selectedIndex = k;
-					return
-				}
-			});
+        setDefaultOpt: function () {
+            var selectedIndex = this.getDefaultOpt().index
+            if (selectedIndex !== undefined) {
+                this.setOption(this.selectList.find(".option").eq(selectedIndex))
+            }
+        },
 
-			return defaultOpt;
-		},
+        buildTemplate: function () {
+            var that = this,
+                defaultOpt = this.getDefaultOpt()
+            var template = '<div class="' + CLASS_LIST + ' ' + CLASS_LIST + '-' + that.index + '" index="' + that.index + '">',
+                optionsHtml = ''
+            that.elem.find("option").each(function (k, opt) {
+                var text = $(opt).html() || ''
+                optionsHtml += '<li class="option" >' + text + '</li>'
+            })
+            template += '<span class="' + CLASS_STATUS + '"><span class="' + CLASS_TEXT + '">' + defaultOpt.value + '</span><i class="icon"></i></span>'
+            template += '<ul class="options" >'
+            template += optionsHtml + '</ul></div>'
 
-		setDefaultOpt: function () {
-			var selectedIndex = this.getDefaultOpt().index;
-			if (selectedIndex !== undefined) {
-				this.setOption(this.selectList.find(".option").eq(selectedIndex));
-			}
-		},
+            that.elem.hide().after(template)
 
-		buildTemplate: function () {
-			var that = this,
-				defaultOpt = this.getDefaultOpt();
-			var template = '<div class="' + CLASS_LIST + ' ' + CLASS_LIST + '-' + that.index + '" index="' + that.index +  '">',
-				optionsHtml = '';
-			that.elem.find("option").each(function (k, opt) {
-				var text = $(opt).html() || '';
-				optionsHtml += '<li class="option" >' + text + '</li>';
-			});
-			template += '<span class="' + CLASS_STATUS + '"><span class="' + CLASS_TEXT + '">' + defaultOpt.value + '</span><i class="icon"></i></span>';
-			template += '<ul class="options" >';
-			template += optionsHtml + '</ul></div>';
+            that.setSelectList()
+        },
 
-			that.elem.hide().after(template);
+        setSelectList: function () {
+            this.selectList = this.elem.siblings("." + CLASS_LIST + "-" + this.index)
+        },
 
-			that.setSelectList();
-		},
+        bindEvent: function () {
+            var that = this,
+                selectList = this.selectList
+            var docEventHandle = "click.select" + that.index
 
-		setSelectList: function () {
-			this.selectList = this.elem.siblings("." + CLASS_LIST+"-"+this.index);
-		},
+            selectList.find("." + CLASS_STATUS).on("click", function (event) {
+                var self = this,
+                    index = that.index
+                //阻止事件冒泡
+                that.stopBubble(event)
+                //关闭其他下拉选择组件   
+                $("." + CLASS_LIST).each(function (k, v) {
+                    if (k != index) {
+                        $(v).removeClass(CLASS_ACTIVE)
+                        console.log($(v))
+                        that.fadeOutDown($(v).find(".options"), 300)
+                    }
+                })
 
-		bindEvent: function () {
-			var that = this,
-				selectList = this.selectList;
-			var docEventHandle = "click.select" + that.index;
+                if (selectList.hasClass(CLASS_ACTIVE)) {
+                    that.close()
+                } else {
+                    that.open()
+                    //点击其他区域关闭此组件
+                    $(document).off(docEventHandle);
+                    (function documentHandler() {
+                        $(document).one(docEventHandle, function (event) {
+                            console.log("doc")
+                            var e = event || window.event
+                            if ($(e.target).closest("." + CLASS_STATUS)[0] === self) {
+                                documentHandler()
+                                return
+                            }
+                            that.close()
+                        })
+                    })()
+                }
+            })
 
-			selectList.find("." + CLASS_STATUS).on("click", function (event) {
-				var self = this,
-					index = that.index;
-				//阻止事件冒泡
-				that.stopBubble(event);
-				//关闭其他下拉选择组件   
-				$("." + CLASS_LIST).each(function (k, v) {
-					if (k != index) {
-						$(v).removeClass(CLASS_ACTIVE);
-						console.log($(v))
-						that.fadeOutDown($(v).find(".options"), 300);
-					}
-				});
+            selectList.find(".option").on("click", function () {
+                that.setOption($(this))
+            })
+        },
+        open: function () {
+            var selectList = this.selectList,
+                optionsElem = selectList.find(".options")
 
-				if (selectList.hasClass(CLASS_ACTIVE)) {
-					that.close()
-				} else {
-					that.open();
-					//点击其他区域关闭此组件
-					$(document).off(docEventHandle);
-					;(function documentHandler() {
-						$(document).one(docEventHandle, function (event) {
-							console.log("doc")
-							var e = event || window.event;
-							if ($(e.target).closest("." + CLASS_STATUS)[0] === self) {
-								documentHandler();
-								return;
-							}
-							that.close()
-						})
-					})()
-				}
-			});
+            selectList.addClass(CLASS_ACTIVE)
+            this.fadeInUp(optionsElem)
 
-			selectList.find(".option").on("click", function () {
-				that.setOption($(this));
-			})
-		},
-		open:function () {
-			var selectList = this.selectList,
-				optionsElem = selectList.find(".options");
+        },
+        close: function (time) {
+            var selectList = this.selectList,
+                time = time || 300,
+                optionsElem = selectList.find(".options")
 
-			selectList.addClass(CLASS_ACTIVE);
-			this.fadeInUp(optionsElem);
+            selectList.removeClass(CLASS_ACTIVE)
+            this.fadeOutDown(optionsElem, 300)
+        },
+        fadeInUp: function (elem) {
+            if (!elem.length) {
+                return
+            }
 
-		},
-		close:function (time) {
-			var selectList = this.selectList,
-				time=time || 300,
-				optionsElem = selectList.find(".options");
+            elem.show()
+            setTimeout(() => {
+                elem.css({
+                    "transform": "translateY(0)",
+                    "opacity": "1"
+                })
+            }, 0)
 
-			selectList.removeClass(CLASS_ACTIVE);
-			this.fadeOutDown(optionsElem, 300);
-		},
-		fadeInUp: function (elem) {
-			if (!elem.length) {
-				return
-			}
 
-			elem.show().css({
-				"transform": "translateY(0)",
-				"opacity": "1"
-			});
+        },
+        fadeOutDown: function (elem, time) {
+            if (!elem.length) {
+                return
+            }
 
-		},
-		fadeOutDown: function (elem, time) {
-			if (!elem.length) {
-				return
-			}
-			elem.css({
-				"transform": "translateY(50%)",
-				"opacity": "0"
-			});
-			setTimeout(function () {
-				elem.hide()
-			}, time)
-		},
-		setOption: function (target) {
-			var that = this,
-				config = this.config,
-				elem = this.elem,
-				selectList = this.selectList;
-			var optionIndex = target.index(),
-				optionsElem = selectList.find(".options"),
-				optionItemElem = elem.find("option").eq(optionIndex);
+            elem.css({
+                "transform": "translateY(-50%)",
+                "opacity": "0"
+            })
 
-			target.addClass("selected").siblings(".option").removeClass("selected");
+            setTimeout(function () {
+                elem.hide()
+            }, time)
+        },
+        setOption: function (target) {
+            var that = this,
+                config = this.config,
+                elem = this.elem,
+                selectList = this.selectList
+            var optionIndex = target.index(),
+                optionsElem = selectList.find(".options"),
+                optionItemElem = elem.find("option").eq(optionIndex)
 
-			selectList.removeClass(CLASS_ACTIVE).find("." + CLASS_TEXT).text(target.text());
-			that.fadeOutDown(optionsElem, 300);
+            target.addClass("selected").siblings(".option").removeClass("selected")
 
-			//设置select状态，并触发change事件   
-			optionItemElem.attr("selected", true).siblings("option").removeAttr("selected");
+            selectList.removeClass(CLASS_ACTIVE).find("." + CLASS_TEXT).text(target.text())
+            that.fadeOutDown(optionsElem, 300)
 
-			if (typeof config.onChange === "function") {
-				config.onChange.call(that, optionItemElem)
-			} else {
-				elem.trigger("change")
-			}
-		}
-	}
+            //设置select状态，并触发change事件   
+            optionItemElem.attr("selected", true).siblings("option").removeAttr("selected")
 
-	Select.inherits(Skeleton);
+            if (typeof config.onChange === "function") {
+                config.onChange.call(that, optionItemElem)
+            } else {
+                elem.trigger("change")
+            }
+        }
+    }
 
-	Skeleton.select = function (options) {
-		return new Select(options)
-	}
+    Select.inherits(Skeleton)
 
-})(Skeleton, jQuery);
+    Skeleton.select = function (options) {
+        return new Select(options)
+    }
+
+})(Skeleton, jQuery)
